7b577fa0e80e660325910881409965596849cf0d
eladkah <elad.kahlon@gmail.com>
2016-07-08 17:00:09 +0300

    Solution for: sol-python-verint.modules-lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,32 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+'''
+Searches in directory recursively for files larger than specific size and asks user if he wants to delete them.
+Receives the directory and minimum size of file as script parameters.
+Usage: python 03.py <directoryPath> <minimumFileSizeInKb>
+'''
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
+import sys
+import os
 
-- Take threshold and path as command line arguments
-"""
+if len(sys.argv) != 3 or not sys.argv[2].isdigit():
+    print 'Usage: python 03.py <directoryPath> <minimumFileSizeInKb>'
+    sys.exit(1)
 
+directory = sys.argv[1]
+minSizeInKb = int(sys.argv[2])
+
+if not os.path.exists(directory):
+    print '%s does not exists' % directory
+    sys.exit(1)
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        sizeInKb = os.stat(root + "\\" + file).st_size / 1024.0
+        filePath = os.path.join(root, file)
+        if sizeInKb >= minSizeInKb:
+            print '%s size is: %f. Do you want to delete it? (yes/no)' % (filePath, sizeInKb)
+            answer = raw_input()
+            while answer not in ('yes', 'no'):
+                print 'Please enter yes or no'
+                answer = raw_input()
+            if answer == 'yes':
+                os.remove(filePath)
