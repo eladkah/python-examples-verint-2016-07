a1d775c0a7d614a658cc1977e5cd9762f3df6a2d
eladkah <elad.kahlon@gmail.com>
2016-07-08 17:00:11 +0300

    Solution for: sol-python-verint.modules-lab/03_bonus.py

--- 06_modules_lab/03_bonus.py
+++ 06_modules_lab/03_bonus.py
@@ -1,13 +1,34 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+'''
+Searches in directory recursively for files larger than specific size and asks user if he wants to delete them.
+Receives the directory and minimum size of file as script parameters. Uses argparse module to read the parameters.
+Usage: python 03.py <directoryPath> <minimumFileSizeInKb>
+'''
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
+import sys
+import os
+import argparse
 
-- Take threshold and path as command line arguments
+parser = argparse.ArgumentParser(description='Find big files')
+parser.add_argument('directory', metavar='D', help="Root directory to search")
+parser.add_argument('minSizeInKb', metavar='S', type=int, help="Minimum file size in Kbytes")
+args = parser.parse_args()
 
-Bonus: Use argparse module to parse command line arguments
-"""
+directory = args.directory
+minSizeInKb = args.minSizeInKb
 
+if not os.path.exists(directory):
+    print '%s does not exists' % directory
+    sys.exit(1)
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        sizeInKb = os.stat(root + "\\" + file).st_size / 1024.0
+        filePath = os.path.join(root, file)
+        if sizeInKb >= minSizeInKb:
+            print '%s size is: %f. Do you want to delete it? (yes/no)' % (filePath, sizeInKb)
+            answer = raw_input()
+            while answer not in ('yes', 'no'):
+                print 'Please enter yes or no'
+                answer = raw_input()
+            if answer == 'yes':
+                os.remove(filePath)
