00a14050378ff028348b70ab268e432c76620485
eladkah <elad.kahlon@gmail.com>
2016-07-12 16:18:32 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,17 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
+'''
+Usage: 01.py <targetFile> <sourceFile>
+Appends the content of <sourceFile> to the end of <targetFile>
+'''
 
+import sys
+import os
+
+if len(sys.argv) != 3 or not os.path.exists(sys.argv[1]) or not os.path.exists(sys.argv[2]):
+    print 'Usage: %s <targetFile> <sourceFile>' % sys.argv[0]
+    sys.exit(1)
+
+with open(sys.argv[1], 'a') as target_file:
+    target_file.write('\n')
+    with open (sys.argv[2], 'r') as source_file:
+        for line in source_file:
+            target_file.write(line)
