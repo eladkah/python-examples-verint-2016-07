fcbdbc00ad4fce6f59a45c9a1f09723b843ed31c
eladkah <elad.kahlon@gmail.com>
2016-07-16 18:39:17 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,6 @@
-"""
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
-"""
+'''
+prints all 3 small english letters combinations
+'''
 
+small_letters = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+print [a+b+c for a in small_letters for b in small_letters for c in small_letters]
