f602e7e2da8e5f361e0f96450cf49a093d80e2ee
eladkah <elad.kahlon@gmail.com>
2016-07-16 19:09:25 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,19 @@
-"""
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
-"""
+'''
+mysum calcs sum of parameters while ignoring non-int items
+mymul calcs mul of parameters while ignoring non-int items
+'''
 
+
+def mysum(*n):
+    return sum(i for i in n if type(i) is int)
+
+
+def mymul(*n):
+    mul = 1
+    for j in [i for i in n if type(i) is int]:
+        mul = mul * j
+    return mul
+
+
+print mysum('a', 'b', 10, 20)
+print mymul('a', 'b', 10, 20)
