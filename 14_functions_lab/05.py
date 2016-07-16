708a958684600c70fd5dc4a6b121c78d54ed540e
eladkah <elad.kahlon@gmail.com>
2016-07-16 19:09:35 +0300

    Solution for: sol-python-verint.functions-lab/05.py

--- 14_functions_lab/05.py
+++ 14_functions_lab/05.py
@@ -1,10 +1,16 @@
-"""
-Write a function groupby that takes a list
-and a function and returns a dictionary
-keyd by the return value of the function on the list items
+'''
+returns dictionary where key is result of function and value is list of strings that returned the same result
+'''
 
-For example:
-    groupby(lambda s: s[0], ['foo', 'fi', 'hello', 'hi'])
-    returns: { 'f': ['foo','fi'], 'h': ['hello', 'hi'] }
-"""
+def group_by(s, *words):
+    dictionary = {}
+    for word in words:
+        key = s(word)
+        if key in dictionary:
+            dictionary[s(word)].append(word)
+        else:
+            dictionary[s(word)] = [word]
 
+    return dictionary
+
+print group_by(lambda(s): s[0], 'hello', 'hi', 'help', 'bye', 'here')
