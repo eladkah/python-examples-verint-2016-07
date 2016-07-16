eb1409320680a2f32f412f2b4493c512a5fa0812
eladkah <elad.kahlon@gmail.com>
2016-07-16 19:09:30 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,8 @@
-"""
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
-"""
+'''
+Calculates the sum of the tens digit of the numbers
+'''
 
+def calc(*n):
+     return sum(int(str(number)[-2]) for number in n if type(number) is int and number >= 10)
+
+print calc(1120, 220, 140, 1, 'a')
