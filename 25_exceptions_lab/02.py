85c458f5334cc3b4bf13c73b9680e8aaceb5a04a
eladkah <elad.kahlon@gmail.com>
2016-07-27 21:41:59 +0300

    Solution for: sol-python-verint.exceptions-lab/02.py

--- 25_exceptions_lab/02.py
+++ 25_exceptions_lab/02.py
@@ -1,7 +1,19 @@
-"""
-Write a program that takes a file name
-and prints line count for the file
+'''
+Gets a file as a parameter and prints its number of lines
+Uses exceptions to tell if no parameters or file does not exist
+'''
 
-Alert the user politely if there was any problem opening the file
-"""
+import sys
+import os
 
+try:
+    if len(sys.argv) != 2:
+        raise Exception('Invalid parameters. Expected %s <file_name>' % sys.argv[0])
+    if not os.path.exists(sys.argv[1]):
+        raise Exception('Sorry, file: %s not found' % sys.argv[1])
+    with open(sys.argv[1], 'r') as f:
+        lines = sum(1 for line in f)
+        print lines
+
+except Exception as e:
+    print e.message
